865426208d1e3a5975bf3df7eea5c61e63df2cf2
Metehan Gültekin <Metehan Gltekin>
2017-04-24 23:31:05 +0300

    app.py file added, this is the main file of the project

---
+++ app.py
@@ -0,0 +1,27 @@
+import os
+import sys
+import json
+import urllib
+import requests
+from flask import Flask, request
+
+
+app = Flask(__name__)
+origin = {'init_key':'init_val'}
+destination = {'init_key':'init_val'}
+date = {'init_key':'init_val'}
+@app.route('/', methods=['GET'])
+def verify():
+    # when the endpoint is registered as a webhook, it must echo back
+    # the 'hub.challenge' value it receives in the query arguments
+    if request.args.get("hub.mode") == "subscribe" and request.args.get("hub.challenge"):
+        if not request.args.get("hub.verify_token") == os.environ["VERIFY_TOKEN"]:
+            return "Verification token mismatch", 403
+        return request.args["hub.challenge"], 200
+
+    return "Hello world", 200
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
