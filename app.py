3d757435dc15e22d9c303d5d606037b56063a6e3
Metehan Gültekin <Metehan Gltekin>
2017-04-25 11:32:00 +0300

    satisfied pylint by disabling warning messages or fixing them

--- app.py
+++ app.py
@@ -1,3 +1,9 @@
+# pylint: disable-msg=C0111
+# pylint: disable-msg=C0103
+# pylint: disable-msg=W0603
+# pylint: disable-msg=W0612
+# pylint: disable-msg=C0301
+
 import os
 import sys
 import json
@@ -6,14 +12,16 @@ import requests
 from flask import Flask, request
 
 
+
 app = Flask(__name__)
 origin = {'init_key':'init_val'}
 destination = {'init_key':'init_val'}
 date = {'init_key':'init_val'}
+
 @app.route('/', methods=['GET'])
 def verify():
-    # when the endpoint is registered as a webhook, it must echo back
-    # the 'hub.challenge' value it receives in the query arguments
+    """ when the endpoint is registered as a webhook, it must echo back
+    the 'hub.challenge' value it receives in the query arguments"""
     if request.args.get("hub.mode") == "subscribe" and request.args.get("hub.challenge"):
         if not request.args.get("hub.verify_token") == os.environ["VERIFY_TOKEN"]:
             return "Verification token mismatch", 403
@@ -24,33 +32,29 @@ def verify():
 
 @app.route('/', methods=['POST'])
 def webhook():
-
-    # endpoint for processing incoming messaging events
+    """To link the webhook with our webserver. This checks if there is a new message or not"""
+    data = request.get_json()
+    log(data)
     global origin
     global destination
     global date
-    data = request.get_json()
-    log(data)  # you may not want to log every incoming message in production, but it's good for testing
-
     if data["object"] == "page":
-
         for entry in data["entry"]:
             for messaging_event in entry["messaging"]:
-
-                if messaging_event.get("message"):  # someone sent us a message
-                    sender_id = messaging_event["sender"]["id"]        # the facebook ID of the person sending you the message
-                    recipient_id = messaging_event["recipient"]["id"]  # the recipient's ID, which should be your page's facebook ID
-                    message_text = messaging_event["message"]["text"]  # the message's text
+                if messaging_event.get("message"):  # trigger when a message is sent to us
+                    sender_id = messaging_event["sender"]["id"]        # the message sender's Facebook ID
+                    recipient_id = messaging_event["recipient"]["id"]  # Facebook ID of our page
+                    message_text = messaging_event["message"]["text"]  # the message itself
                     paramsList = message_text.split(",")
                     try:
                         origin[sender_id] = paramsList[0]
                         destination[sender_id] = paramsList[1]
                         date[sender_id] = paramsList[2]
-                    except:
+                    except ValueError:
                         log("Index error")
                     try:
                         url = "http://partners.api.skyscanner.net/apiservices/browsequotes/v1.0/TR/try/en-GB/"+origin[sender_id]+"/"+destination[sender_id]+"/"+date[sender_id]+"/?apikey=prtl6749387986743898559646983194"
-                        response = urllib.urlopen(url)  
+                        response = urllib.request.urlopen(url)
                         data = json.loads(response.read())
                         log(data)
                         planenames = ""
@@ -61,26 +65,24 @@ def webhook():
                                         for plane in data["Carriers"]:
                                             if plane["CarrierId"] == cid:
                                                 planenames = str(plane["Name"])
-                                    except:
+                                    except ValueError:
                                         log("plane")
-                            except:
+                            except ValueError:
                                 log("cid")
                             datetime = str(quote["OutboundLeg"]["DepartureDate"]).split('T')
                             if datetime[1] == "00:00:00":
                                 send_message(sender_id, str(planenames) + " \nDate: " + datetime[0] + " \nPrice: " +str(quote["MinPrice"])+"TL")
                             else:
                                 send_message(sender_id, str(planenames) + " \nDate: " + datetime[0] + " \nTime: " + datetime[1] + " \nPrice: " +str(quote["MinPrice"])+"TL")
-                    except:
+                    except ValueError:
                         log("Session error")
-                
-                
-                if messaging_event.get("delivery"):  # delivery confirmation
+                if messaging_event.get("delivery"):
                     pass
 
-                if messaging_event.get("optin"):  # optin confirmation
+                if messaging_event.get("optin"):
                     pass
 
-                if messaging_event.get("postback"):  # user clicked/tapped "postback" button in earlier message
+                if messaging_event.get("postback"):
                     pass
 
     return "ok", 200
@@ -88,7 +90,7 @@ def webhook():
 
 
 def send_message(recipient_id, message_text):
-
+    """To send the message_text to someone with recipient_id"""
     log("sending message to {recipient}: {text}".format(recipient=recipient_id, text=message_text))
 
     params = {
@@ -105,14 +107,15 @@ def send_message(recipient_id, message_text):
             "text": message_text
         }
     })
-    r = requests.post("https://graph.facebook.com/v2.6/me/messages", params=params, headers=headers, data=data)
-    if r.status_code != 200:
-        log(r.status_code)
-        log(r.text)
+    req = requests.post("https://graph.facebook.com/v2.6/me/messages", params=params, headers=headers, data=data)
+    if req.status_code != 200:
+        log(req.status_code)
+        log(req.text)
 
 
-def log(message):  # simple wrapper for logging to stdout on heroku
-    print str(message)
+def log(message): 
+    """To put the errors and valuable data into logs"""
+    print(message)
     sys.stdout.flush()
 
 
